1a16eedb4757aaf78aff01a9f577e756f86f94a6
Ervan Kurniawan <43870656+ervankrnwn41@users.noreply.github.com>
2020-12-09 21:31:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,11 @@ echo "[1] Create .termux directory at HOME"
 mkdir -p $HOME/.termux
 sleep 1
 echo
-echo "[2] Install cURL"
-apt install curl -y
-sleep 2
-echo
-echo "[3] Time to Modified the Navigation"
+echo "[2] Time to Modified the Navigation"
 echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> $_/termux.properties
 sleep 2
 echo
-echo "[4] Let\'s reload termux"
+echo "[3] Let\'s reload termux"
 termux-reload-settings
 sleep 2
 echo
